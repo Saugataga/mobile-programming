fe89cbefe447d39416f00c4af439d79595579d60
Saugataga <saugataga121@gmail.com>
2026-09-08 08:14:01 +0545

    (Feat) Added Subtraction and Multiplication

--- week-2/script.js
+++ week-2/script.js
@@ -1,7 +1,24 @@
- function addNumber(){
-        let number1 = Number(document.getElementById("number").value);
-        let number2 = Number(document.getElementById("number2").value);
-        let sum = number1 + number2;
-        
-        document.getElementById("result").innerText = "The sum is: " + sum;
-   }
+function addNumber() {
+  let number1 = Number(document.getElementById("number").value);
+  let number2 = Number(document.getElementById("number2").value);
+  let sum = number1 + number2;
+
+  document.getElementById("result").innerText = "The sum is: " + sum;
+}
+
+function subtractNumber() {
+  let number1 = Number(document.getElementById("number").value);
+  let number2 = Number(document.getElementById("number2").value);
+  let difference = number1 - number2;
+
+  document.getElementById("result2").innerText =
+    "The difference is: " + difference;
+}
+
+function multiplyNumber() {
+  let number1 = Number(document.getElementById("number").value);
+  let number2 = Number(document.getElementById("number2").value);
+  let product = number1 * number2;
+
+  document.getElementById("result3").innerText = "The product is: " + product;
+}
