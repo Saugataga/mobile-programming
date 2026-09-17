550f538792c6886e58c491e813dd09ea2b4d8952
Saugataga <saugataga121@gmail.com>
2026-09-17 08:11:33 +0545

    Week 2 Stuff

--- week-3/task.js
+++ week-3/task.js
@@ -32,20 +32,76 @@ $("#highlight-card").click(function() {
 });
 
 $("#remove-highlight").click(function() {
-    $("#profile-card").removeClass("highlighted");
-    
+    $("#profile-card").removeClass("highlighted").css("background-color", "");
 });
 
 $("#dark-mode").click(function() {
     $("#profile-card").toggleClass("dark-mode");
-    $(".dark-mode").css({
-        "background-color": "#333",
-        "color": "#fff"
-    });
 });
 
 $("#toggle-round").click(function() {
-    $("#profile-card").toggleClass("rounded");
+    $("#profile-photo").toggleClass("rounded");
 });
 
+$("#profile-photo").mouseenter(function() {
+    $(this).addClass("shadow");
+});
+
+$("#profile-photo").mouseleave(function() {
+    $(this).removeClass("shadow");
+});
+
+$("#nickname-input").keydown(function(event) {
+    $("#output").text(event.key);
+});
+
+$("#red").click(function() {
+    $("#profile-card").css("background-color", "red");
+});
+
+$("#reset").click(function() {
+    $("#profile-card").css("background-color", "");
+});
+
+$("#hide-photo").click(function() {
+    $("#profile-photo").hide("slow");
+});
+
+$("#show-photo").click(function() {
+    $("#profile-photo").show("slow");
+});
+
+$("#toggle-bio").click(function() {
+    $("#bio").toggle();
+});
+
+$("#fade-out").click(function() {
+    $("#profile-card").fadeOut();
+});
+
+$("#fade-in").click(function() {
+    $("#profile-card").fadeIn();
+});
+
+$("#fade-50").click(function() {
+    $("#profile-card").fadeTo(500, 0.5);
+});
+
+$("#slide-up-skills").click(function() {
+    $("#skills-list").slideUp();
+});
+
+$("#slide-down-skills").click(function() {
+    $("#skills-list").slideDown();
+});
+
+$("#slide-toggle-skills").click(function() {
+    $("#skills-list").slideToggle();
+});
+
+$("#animate-card").click(function() {
+    $("#profile-card")
+        .animate({"margin-left": "200px"}, 1000)
+        .animate({"margin-left": "0px"}, 1000);
+});
 })
